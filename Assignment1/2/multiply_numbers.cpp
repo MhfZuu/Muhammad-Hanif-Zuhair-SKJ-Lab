939e88d65c9912cd4a04e8bc1ce9320bc69b21f5
MhfZuu <muhammadhanifzuhair2005@mail.ugm.ac.id>
2024-08-30 09:30:28 +0700

    Update and add some files

---
+++ Assignment1/2/multiply_numbers.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int num1 = 5;
+    int num2 = 10;
+    int result;
+
+    result = num1 * num2;
+
+    cout << "Result: " << result << std::endl;
+
+    return 0;
+}
